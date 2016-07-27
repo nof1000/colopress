5d9623148257427a5e1dc52eeb6250f7a1fc63d6
Denis Maslennikov <nof1000@mail.ru>
2016-07-27 13:57:42 +0300

    Added Config & Clean Script

---
+++ config/clean.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const fs = require('fs');
+
+const cfg = require('./config.js');
+
+function clean(dir) {
+    let current;
+
+    if (fs.statSync(dir)) {
+        fs.readdirSync(dir).forEach(file => {
+            current = path.join(dir, file);
+
+            if (fs.statSync(current).isDirectory()) {
+                return clean(current);
+            }
+
+            fs.unlinkSync(current);
+        });
+
+        fs.rmdirSync(dir);
+    }
+};
+
+try {clean(cfg.dir.build)} catch (e) {}
+console.log('all cleaned');
---
+++ config/config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const pkg = require('../package.json');
+
+module.exports = {
+    name: pkg.name,
+    version: pkg.version,
+    public: '/',
+    dir: {
+        modules: path.resolve(__dirname, '../node_modules'),
+        build: path.resolve(__dirname, '../build'),
+        root: path.resolve(__dirname, '../'),
+        app: path.resolve(__dirname, '../app')
+    }
+};
